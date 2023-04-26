dc6f98d121d11edbc3e372d7de482b69d7548a5f
LookasWasTaken~ <piacen.luca@gmail.com>
2023-04-26 18:38:09 +0200

    ADD logo components

--- src/components/JumboTron.vue
+++ src/components/JumboTron.vue
@@ -1,8 +1,12 @@
 <script>
 import { state } from "../assets/data/state";
+import nexgenLogo from "../components/nexgenLogo.vue"
 
 export default {
     name: 'JumboTron',
+    components: {
+        nexgenLogo,
+    },
     data() {
         return {
             state
@@ -13,6 +17,33 @@ export default {
 
 <template>
     <div class="jumbotron">
-        
+        <div class="overlay">
+            <div class="container d-flex justify-content-between align-items-center py-5">
+                <div class="left align-self-start">
+                    <nexgenLogo></nexgenLogo>
+                </div>
+                <div class="right">
+                    <div class="menu">
+                        <ul class="list-unstyled d-flex justify-content-between align-items-center">
+                            <li>HOME</li>
+                            <li>SERVICES</li>
+                            <li>SKILLS</li>
+                            <li>PARTNERS</li>
+                            <li>BLOG</li>
+                            <li>GET IN TOUCH</li>
+                        </ul>
+                    </div>
+                    <div class="info w-50 d-flex flex-column gap-3 my-5">
+                        <h1 class="my-3"><div class="logistics">Logistics</div>
+                             that goes further.</h1>
+                        <p class="my-3">For 20 years working with the most innovative in the field of transport.</p>
+                        <div class="buttons my-3 d-flex justify-content-between align-items-center">
+                            <button>GET IN TOUCH</button>
+                            <button>READ MORE</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
---
+++ src/components/nexgenLogo.vue
@@ -0,0 +1,21 @@
+<script>
+
+export default {
+    name: 'nexgenLogo',
+}
+</script>
+
+<template>
+    <div class="logo d-flex justify-content-center align-items-center gap-3">
+        <div class="nex d-flex gap-4">
+            <span>N</span>
+            <span>E</span>
+            <span>X</span>
+        </div>
+        <div class="gen d-flex gap-4">
+            <span>G</span>
+            <span>E</span>
+            <span>N</span>
+        </div>
+    </div>
+</template>
