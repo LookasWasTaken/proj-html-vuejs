f0b9588d5f3911bf323d816be16e8040c698b8a9
LookasWasTaken~ <piacen.luca@gmail.com>
2023-04-27 16:24:29 +0200

    ADD dinamic banner section

---
+++ src/assets/data/banner.js
@@ -0,0 +1,9 @@
+export const banner = [
+    "./src/assets/img/logo-5.png",
+    "./src/assets/img/logo-1.png",
+    "./src/assets/img/logo-2.png",
+    "./src/assets/img/logo-3.png",
+    "./src/assets/img/logo-4.png",
+    "./src/assets/img/logo-5.png",  
+  ];
+  
--- src/components/BannerSection.vue
+++ src/components/BannerSection.vue
@@ -1,11 +1,13 @@
 <script>
 import { state } from '../assets/data/state';
+import { banner } from '../assets/data/banner';
 
 export default {
     name: 'BannerSection',
     data() {
         return {
-            state
+            state,
+            banner,
         }
     },
 }
@@ -14,12 +16,7 @@ export default {
 <template>
     <section id="banner" class="d-flex">
         <div class="container d-flex">
-            <img class="img-fluid" src="../assets/img/logo-5.png" alt="">
-            <img class="img-fluid" src="../assets/img/logo-1.png" alt="">
-            <img class="img-fluid" src="../assets/img/logo-2.png" alt="">
-            <img class="img-fluid" src="../assets/img/logo-3.png" alt="">
-            <img class="img-fluid" src="../assets/img/logo-4.png" alt="">
-            <img class="img-fluid" src="../assets/img/logo-5.png" alt="">
+            <img v-for="(img, index) in banner" class="img-fluid" :src="img" :alt="`logo ${index}`">
         </div>
     </section>
 </template>
