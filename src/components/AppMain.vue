7afac5605b3d4bbf20b436af67a359cd06589799
LookasWasTaken~ <piacen.luca@gmail.com>
2023-04-27 16:07:21 +0200

    ADD banner section

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,12 +2,14 @@
 import { state } from "../assets/data/state";
 import ServiceSection from "./ServiceSection.vue"
 import NetworkSection from "./NetworkSection.vue"
+import BannerSection from "./BannerSection.vue"
 
 export default {
     name: 'AppMain',
     components: {
         ServiceSection,
         NetworkSection,
+        BannerSection,
     },
     data() {
         return {
@@ -21,5 +23,6 @@ export default {
     <main>
         <ServiceSection></ServiceSection>
         <NetworkSection></NetworkSection>
+        <BannerSection></BannerSection>
     </main>
 </template>
---
+++ src/components/BannerSection.vue
@@ -0,0 +1,25 @@
+<script>
+import { state } from '../assets/data/state';
+
+export default {
+    name: 'BannerSection',
+    data() {
+        return {
+            state
+        }
+    },
+}
+</script>
+
+<template>
+    <section id="banner" class="d-flex">
+        <div class="container d-flex">
+            <img class="img-fluid" src="../assets/img/logo-5.png" alt="">
+            <img class="img-fluid" src="../assets/img/logo-1.png" alt="">
+            <img class="img-fluid" src="../assets/img/logo-2.png" alt="">
+            <img class="img-fluid" src="../assets/img/logo-3.png" alt="">
+            <img class="img-fluid" src="../assets/img/logo-4.png" alt="">
+            <img class="img-fluid" src="../assets/img/logo-5.png" alt="">
+        </div>
+    </section>
+</template>
